2d01ea43aca433a2a78f9399ce0ac89e7cfe9f11
Neo Gravity <neogravitypawn@outlook.com>
2025-07-09 18:47:40 +0200

    Complete basic setup

---
+++ src/backend/index.ts
@@ -0,0 +1,11 @@
+import { rootRequestHandler } from "#be/handler/requestHandler.js";
+import express from "express";
+
+const app = express();
+const port = process.env.PORT ?? "9001";
+
+app.get("/", rootRequestHandler);
+
+app.listen(port, () => {
+  console.log(`Listen to port ${port}`);
+});

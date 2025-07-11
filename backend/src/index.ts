7ff086d6e72ef5154eb4cbb9ef6c05b96401c4a1
Neo Gravity <neogravitypawn@outlook.com>
2025-07-11 20:09:18 +0200

    Create basic frontend svelte setup and integrate into backend dist

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
-import { rootRequestHandler } from "#handler/requestHandler.js";
+//import { rootRequestHandler } from "#handler/requestHandler.js";
 import express from "express";
 
 const app = express();
 const port = process.env.PORT ?? "9001";
 
-app.get("/", rootRequestHandler);
+app.use(express.static("dist/src/www"));
+
+//app.get("/", rootRequestHandler);
 
 app.listen(port, () => {
   console.log(`Listen to port ${port}`);
